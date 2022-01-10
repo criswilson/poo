60eb8d2e30e461efefaac6d5b7c7e54f6e8b6307
cristian <cristian.wilson3@gmail.com>
2022-01-10 01:41:13 -0300

    segundo commit

--- codigo.js
+++ codigo.js
@@ -1,32 +1,123 @@
-class Animal {
-  constructor(especie, edad, color) {
-    this.especie = especie;
-    this.edad = edad;
+class Celulares {
+  constructor(color, peso, resPantalla, resCamara, ram) {
     this.color = color;
-    this.info = `Soy ${this.especie}, tengo ${this.edad} años y soy de color ${this.color}`;
+    this.peso = peso;
+    this.resolucionDePantalla = resPantalla;
+    this.resolucionDeCamara = resCamara;
+    this.memoriaRam = ram;
+    this.encendido = false;
   }
-  verInfo() {
-    document.write(this.info + `<br>`);
+
+  presionarBotonEncendido() {
+    if (this.encendido == false) {
+      alert(`Celular prendido`);
+      this.encendido = true;
+    } else {
+      this.encendido = false;
+      alert(`Celular apagado`);
+    }
+  }
+
+  reiniciar() {
+    if (this.encendido == true) {
+      alert(`Reiniciando celular`);
+    } else {
+      alert(`El celular está apagado`);
+    }
+  }
+
+  tomarFoto() {
+    if (this.encendido == true) {
+      alert(`Foto tomada en una resolución de: ${this.resolucionDeCamara}`);
+    } else {
+      alert(`El celular está apagado`);
+    }
+  }
+
+  grabarVideo() {
+    if (this.encendido == true) {
+      alert(`Grabando video en: ${this.resolucionDeCamara}`);
+    } else {
+      alert(`El celular está apagado`);
+    }
+  }
+
+  mobileInfo() {
+    return `
+        color: <b>${this.color}</b> <br>
+        peso: <b>${this.peso}</b> <br>
+        resolución de pantalla: <b>${this.resolucionDePantalla}</b> <br>
+        Memoria RAM: <b>${this.memoriaRam}</b> <br>
+        resolución de cámara: <b>${this.resolucionDeCamara}</b> <br>
+      `;
   }
 }
 
-class Perro extends Animal {
-  constructor(especie, edad, color, raza) {
-    super(especie, edad, color);
-    this.raza = null;
-  }
-  set setRaza(newName) {
-    this.raza = newName;
+class CelularesAltaGama extends Celulares {
+  constructor(color, peso, resPantalla, resCamara, ram, camExtra) {
+    super(color, peso, resPantalla, resCamara, ram);
+    this.resolucionCamaraExtra = camExtra;
+    this.encendido = false;
   }
 
-  get getRaza() {
-    return this.raza;
+  grabarVideoSlowCam() {
+    if (this.encendido == true) {
+      alert(`Grabando video en cámara lenta en: ${this.resolucionDeCamara}`);
+    } else {
+      alert(`El celular está apagado`);
+    }
+  }
+
+  recFacial() {
+    if (this.encendido == true) {
+      alert(`Reconocimiento facial activado`);
+    } else {
+      alert(`El celular está apagado`);
+    }
+  }
+
+  infoAltaGama() {
+    return (
+      this.mobileInfo() +
+      `resolución de cámara extra: <b>${this.resolucionCamaraExtra}</b> <br>`
+    );
   }
 }
 
-const perro = new Perro(`perro`, 5, `marrón`, `doberman`);
-const gato = new Animal(`gato`, 2, `negro`);
-const pajaro = new Animal(`pájaro`, 1, `verde`);
+const celular1 = new Celulares(`Rojo`, `150g`, `5'`, `HD`, `1GB`);
+const celular2 = new Celulares(`Negro`, `155g`, `5.4'`, `full HD`, `2GB`);
+const celular3 = new Celulares(`Blanco`, `146g`, `5.9'`, `full HD`, `2GB`);
+const celular4 = new CelularesAltaGama(
+  `Rojo`,
+  `130g`,
+  `5.2'`,
+  `4K`,
+  `3GB`,
+  `full HD`
+);
+const celular5 = new CelularesAltaGama(
+  `Negro`,
+  `142g`,
+  `6'`,
+  `4K`,
+  `4GB`,
+  `HD`
+);
 
-perro.setRaza = "Pedro";
-document.write(perro.getRaza);
+// celular1.presionarBotonEncendido();
+// celular1.tomarFoto();
+// celular1.grabarVideo();
+// celular1.reiniciar();
+// celular1.presionarBotonEncendido();
+
+document.write(`
+${celular1.mobileInfo()} <br>
+${celular2.mobileInfo()} <br>
+${celular3.mobileInfo()} <br>
+${celular4.infoAltaGama()} <br>
+${celular5.infoAltaGama()} <br>
+`);
+
+celular4.presionarBotonEncendido();
+celular4.recFacial();
+celular4.grabarVideoSlowCam();
